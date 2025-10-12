9106c2857b0b50e2ab996e5208614b938a3bedb3
Joel <bjeferssonvinicio2005@gmail.com>
2025-10-11 20:04:05 -0500

    feat: add model definitions

---
+++ src/main/java/com/gpis/marketplace_link/entities/PublicationImage.java
@@ -0,0 +1,23 @@
+package com.gpis.marketplace_link.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "publication_images")
+@Data
+public class PublicationImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "publication_id")
+    private Publication publication;
+
+
+}
+
