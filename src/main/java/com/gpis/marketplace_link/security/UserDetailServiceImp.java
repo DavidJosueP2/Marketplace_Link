3545c154ea1e19d5f8a61912261f6a4b28b56c6d
DRTX2 <davidmanjarres2004@gmail.com>
2025-11-30 12:43:36 -0500

    fix(login): exception with correct credentials

--- src/main/java/com/gpis/marketplace_link/security/UserDetailServiceImp.java
+++ src/main/java/com/gpis/marketplace_link/security/UserDetailServiceImp.java
@@ -2,17 +2,13 @@ package com.gpis.marketplace_link.security;
 
 import com.gpis.marketplace_link.entities.User;
 import com.gpis.marketplace_link.repositories.UserRepository;
+import com.gpis.marketplace_link.security.user.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class UserDetailServiceImp implements UserDetailsService {
@@ -22,21 +18,9 @@ public class UserDetailServiceImp implements UserDetailsService {
     @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(("User by username not found.")));
+        User user = userRepository.findByEmailWithRoles(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User by username not found."));
 
-        List<GrantedAuthority> authorities = user.getRoles()
-                .stream()
-                .map((role -> new SimpleGrantedAuthority(role.getName())))
-                .collect(Collectors.toList());
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                true,
-                true,
-                true,
-                true,
-                authorities
-        );
+        return new CustomUserDetails(user);
     }
 }
